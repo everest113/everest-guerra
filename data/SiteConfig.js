44bf905b2e4b20b38b259397b963684ba841ca07
Everest Guerra <everestguerra@Everests-MacBook-Pro-2.local>
2020-05-11 18:46:12 -0400

    updates resume + meta og:image, also moved to New York

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -1,14 +1,15 @@
-const moment = require('moment');
+const moment = require('moment')
 
 module.exports = {
-  siteTitle: "Everest Guerra", // Site title.
-  siteTitleShort: "Everest Guerra", // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
-  siteTitleAlt: "Everest Guerra", // Alternative site title for SEO.
-  siteLogo: "/src/images/text.svg", // Logo used for SEO and manifest.
-  siteUrl: "https://everestguerra.com", // Domain of your website without pathPrefix.
-  siteDescription: "Full-stack developer currently based in Detroit", // Website description used for RSS feeds/meta description tag.
-  googleAnalyticsID: "UA-47311644-5", // GA tracking ID.
+  siteTitle: 'Everest Guerra', // Site title.
+  siteTitleShort: 'Everest Guerra', // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
+  siteTitleAlt: 'Everest Guerra', // Alternative site title for SEO.
+  siteLogo: '/src/images/text.svg', // Logo used for SEO and manifest.
+  metaImage: '/metaImage.png', // Must be statically available
+  siteUrl: 'https://everestguerra.com', // Domain of your website without pathPrefix.
+  siteDescription: 'Full-stack developer currently based in New York', // Website description used for RSS feeds/meta description tag.
+  googleAnalyticsID: 'UA-47311644-5', // GA tracking ID.
   copyright: `Copyright © ${moment().format('YYYY')}. Everest Guerra`, // Copyright string for the footer of the website and RSS feed.
-  themeColor: "#2b2b33", // Used for setting manifest and progress theme colors.
-  backgroundColor: "#fffff", // Used for setting manifest background color.
-};
+  themeColor: '#2b2b33', // Used for setting manifest and progress theme colors.
+  backgroundColor: '#fffff', // Used for setting manifest background color.
+}
--- src/components/SEO/index.jsx
+++ src/components/SEO/index.jsx
@@ -1,26 +1,25 @@
-import React, { Component } from "react";
-import Helmet from "react-helmet";
-import urljoin from "url-join";
-import config from "../../../data/SiteConfig";
+import React, { Component } from 'react'
+import Helmet from 'react-helmet'
+import urljoin from 'url-join'
+import config from '../../../data/SiteConfig'
 
 class SEO extends Component {
+  render() {
+    const { siteTitle, siteDescription, siteUrl } = config
+    const image = urljoin(config.siteUrl, config.metaImage)
 
-	render() {
-		const { siteTitle, siteDescription, siteUrl } = config
-		const image = urljoin(config.siteUrl, config.siteLogo);
+    const schemaOrgJSONLD = [
+      {
+        '@context': 'http://schema.org',
+        '@type': 'WebSite',
+        url: siteUrl,
+        name: siteTitle,
+      },
+    ]
 
-		const schemaOrgJSONLD = [
-			{
-				"@context": "http://schema.org",
-				"@type": "WebSite",
-				url: siteUrl,
-				name: siteTitle
-			}
-		]
-
-		return (
-			<Helmet>
-				{/* General tags */}
+    return (
+      <Helmet>
+        {/* General tags */}
         <meta name="description" content={siteDescription} />
         <meta name="image" content={image} />
 
@@ -40,9 +39,9 @@ class SEO extends Component {
         <meta name="twitter:title" content={siteTitle} />
         <meta name="twitter:description" content={siteDescription} />
         <meta name="twitter:image" content={image} />
-			</Helmet>
-		)
-	}
+      </Helmet>
+    )
+  }
 }
 
-export default SEO;
+export default SEO
